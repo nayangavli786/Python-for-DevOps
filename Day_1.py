5a73af540a86b2279720d63c5491e8b838c364b6
nayangavli786 <NAYANGAVLI786@GMAIL.COM>
2025-02-17 14:35:46 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,15 @@
+#Day 1
+
+#Practice problem 2 = Print area of Square by taking input of its  side
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+sum = a + b
+print("Sum of two numbers is : ", sum)
+print("-----------------------------")
+#Practice problem 2 = Print area of Square by taking input of its  side
+
+side = int(input("Enter the side of square: "))
+Area = side * side
+print("Area of square is: ", Area)
+
+#P
